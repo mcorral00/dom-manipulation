71b73a8ee1f244767400d78653c8350b14266284
Mario Corral <156039623+mcorral00@users.noreply.github.com>
2024-03-27 20:37:52 -0600

    first commit

---
+++ dom.manipulation.js
@@ -0,0 +1,34 @@
+// import "./styles.css";
+
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
+const mainEl = document.querySelector("main");
+
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+
+topMenuEl.style.height = "100%";
+
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+
+topMenuEl.classList.add("flex-around");
+
+// Part three
+
+menuLinks.forEach((m) => {
+  const link = document.createElement("a");
+  link.setAttribute("href", m.href);
+  link.textContent = m.text;
+  topMenuEl.appendChild(link);
+});
